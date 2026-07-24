2e038d54fe33cf111991640605a5a2c98a2f604a
Jonno <blackswansystem@gmail.com>
2026-07-24 11:46:52 +0930

    Grok Attributes

--- src/Form/GrokAiProviderConfigForm.php
+++ src/Form/GrokAiProviderConfigForm.php
@@ -272,7 +272,7 @@ final class GrokAiProviderConfigForm extends ConfigFormBase {
         '#markup' => '<p class="grok-ai-provider-about__supporting-text">' . $this->t('Version: @version', ['@version' => $version]) . '</p>',
       ],
       'framework' => [
-        '#markup' => '<p class="grok-ai-provider-about__supporting-text">' . $this->t('A part of the <a href="https://torneoz.org">Torneoz</a> framework') . '</p>',
+        '#markup' => '<p class="grok-ai-provider-about__supporting-text">' . $this->t('A part of the <a href="https://torneoz.org" title="Torneoz is a global management AI system for Drupal." target="_blank" rel="noopener noreferrer">Torneoz</a> framework') . '</p>',
       ],
       'disclaimer' => [
         '#markup' => '<p class="grok-ai-provider-about__supporting-text">' . $this->t('This project is not affiliated with, funded, or endorsed by SpaceXAi or its subsidiaries. Copyright on all assets is retained by the owners.') . '</p>',
